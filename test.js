6205651583439d01958272311187dd430412e812
Greg Stickel <gregory.stickel@live.com>
2022-09-14 13:02:47 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+for(i=0; i < 20; i+= 2){
+    console.log(i);
+} 
+let catName = "tigger"
+let catAge = 1
+let catColor = "orange"
+
+let x = `The cat's name is ${catName}`;
+console.log(`The cat's name is ${catName}`);
+console.log(x);
---
+++ test2.js
@@ -0,0 +1,12 @@
+let tinyNum = 5
+let hugeNum = 200000
+
+
+for(let i = 0; i < tinyNum; i++){
+    console.log("I am a teeny tiny number");
+}
+if(tinyNum > hugeNum){
+    console.log("I am a teeny tiny number")
+} else {
+    console.log(`I am a huge number, my value is ${hugeNum}`)
+}
